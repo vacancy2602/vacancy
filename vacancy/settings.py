e9dcdb3e99058e6bb2341d4ce4cfaea7066b6eee
unknown <service.tek.bot@mail.ru>
2023-05-25 20:48:51 +0600

    Diploy

--- vacancy/settings.py
+++ vacancy/settings.py
@@ -84,19 +84,20 @@ WSGI_APPLICATION = 'vacancy.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-
     #'default': {
-    #    'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #    'NAME': 'vacancy',
-    #    'USER' : 'customer',
-    #    'PASSWORD' : 'customer',
-    #    'HOST' : '127.0.0.1',
-    #    'PORT' : '5432',
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': BASE_DIR / 'db.sqlite3',
     #}
+
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'vacancy77',
+        'USER' : 'vacancy77_user',
+        'PASSWORD' : 'WOD4lJZzVKPteet62VxF9eqlMW0wtyam',
+        'HOST' : 'dpg-chnn5j9mbg5577n9esdg-a.frankfurt-postgres.render.com',
+        'PORT' : '5432',
+    }
+    
 }
 
 # Password validation
